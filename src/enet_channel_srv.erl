b3cc898a1fc6124c1254efc6494af41669e1014e
cmdrk <cmdrk@protonmail.com>
2022-09-04 14:48:42 -0500

    Fix up some formatting, some log output, clean up the dispatcher code and wrap at the 16-bit limit

--- src/enet_channel_srv.erl
+++ src/enet_channel_srv.erl
@@ -26,6 +26,8 @@
     code_change/3
 ]).
 
+-define(MAX_SEQ_INDEX, 65536).
+
 % records
 -record(state, {
     id,
@@ -126,7 +128,7 @@ handle_cast({send_unreliable, Data}, S0) ->
     N = S0#state.outgoing_unreliable_sequence_number,
     {H, C} = enet_command:send_unreliable(ID, N, Data),
     ok = enet_peer:send_command(Peer, {H, C}),
-    S1 = S0#state{outgoing_reliable_sequence_number = N + 1},
+    S1 = S0#state{outgoing_reliable_sequence_number = next(N)},
     {noreply, S1};
 handle_cast(
     {recv_reliable, {
@@ -142,28 +144,20 @@ handle_cast(
     Worker ! {enet, ID, C},
     % Dispatch any buffered packets
     Window = S0#state.reliable_window,
-    {NextSeq, NewWindow} = dispatch(N, Window, ID, Worker),
+    SortedWindow = lists:keysort(1, Window),
+    {NextSeq, NewWindow} = dispatch(N, SortedWindow, ID, Worker),
     S1 = S0#state{incoming_reliable_sequence_number = NextSeq, reliable_window = NewWindow},
     {noreply, S1};
-% mostly debugging, remove later
-handle_cast(
-    {recv_reliable, {
-        #command_header{reliable_sequence_number = N},
-        _C =
-            #reliable{}
-    }},
-    S0
-) when
+handle_cast({recv_reliable, {#command_header{reliable_sequence_number = N}, _C = #reliable{}}}, S0) when
     N <
         S0#state.incoming_reliable_sequence_number
 ->
-    logger:debug("Got a duplicate reliable packet. Discard"),
+    Expect = S0#state.incoming_reliable_sequence_number,
+    logger:debug("Discard outdated packet. Recv: ~p. Expect: ~p", [N, Expect]),
     {noreply, S0};
 handle_cast({recv_reliable, {#command_header{reliable_sequence_number = N}, C = #reliable{}}}, S0) ->
-    logger:debug("Got reliable packet newer than we expected, buffer"),
-    Window = S0#state.reliable_window,
     Expect = S0#state.incoming_reliable_sequence_number,
-    logger:debug("Got: ~p. Expect: ~p. Window contents: ~p", [N, Expect, Window]),
+    logger:debug("Buffer unexpectedly new packet. Recv: ~p. Expect: ~p.", [N, Expect]),
     ReliableWindow0 = S0#state.reliable_window,
     S1 = S0#state{reliable_window = [{N, C} | ReliableWindow0]},
     {noreply, S1};
@@ -173,7 +167,7 @@ handle_cast({send_reliable, Data}, S0) ->
     N = S0#state.outgoing_reliable_sequence_number,
     {H, C} = enet_command:send_reliable(ID, N, Data),
     ok = enet_peer:send_command(Peer, {H, C}),
-    S1 = S0#state{outgoing_reliable_sequence_number = N + 1},
+    S1 = S0#state{outgoing_reliable_sequence_number = next(N)},
     {noreply, S1};
 handle_cast(Msg, S0) ->
     logger:debug("Unhandled message: ~p", [Msg]),
@@ -190,53 +184,30 @@ terminate(_Reason, _S0) ->
 code_change(_OldVsn, S0, _Extra) ->
     {ok, S0}.
 
-
 % Internal
 %
 
 -spec dispatch(pos_integer(), list(), pos_integer(), pid()) ->
     {pos_integer(), list()}.
-dispatch(CurSeq, Window, ChannelID, Worker) -> 
-    % Sort the window
-    SortedWindow = lists:sort(Window),
-    dispatch(CurSeq, SortedWindow, ChannelID, Worker, 0).
-
-dispatch(CurSeq, [], _ChannelID, _Worker, _Count) ->
-    {CurSeq+1,[]};
-dispatch(CurSeq, Window = [ {Seq1, D1} ], ChannelID, Worker, Count) ->
-    case CurSeq + 1 == Seq1 of 
-        true -> 
-            % Dispatch the packet
-            Worker ! {enet, ChannelID, D1},
-            % Return the next sequence number and the now-empty window.
-            {CurSeq + 1, []};
-        _ ->
-            % The first packet in the window is not the one we're looking for,
-            % so just return.
-            logger:debug("Dispatched ~p packets", [Count]),
-            {CurSeq + 1, Window}
-    end;
-dispatch(CurSeq, Window = [ {Seq1, D1} | RemainingWindow ], ChannelID, Worker, Count) -> 
+dispatch(CurSeq, [], _ChannelID, _Worker) ->
+    NextSeq = next(CurSeq),
+    {NextSeq, []};
+dispatch(CurSeq, Window = [{Seq1, D1} | RemainingWindow], ChannelID, Worker) ->
     % If the buffered item comes immediately after the current sequence number,
-    % dispatch the next packet. 
-    case CurSeq + 1 == Seq1 of 
-        true -> 
+    % dispatch the next packet.
+    NextSeq = next(CurSeq),
+    case NextSeq == Seq1 of
+        true ->
             % Dispatch the packet
+            logger:debug("Dispatching queued packet ~p", [Seq1]),
             Worker ! {enet, ChannelID, D1},
-            % Peak ahead to see if we've got the next one, otherwise just
-            % return the next sequence number
-            [ {Seq2,_D2} | _ ] = RemainingWindow,
-            case CurSeq + 2 == Seq2 of
-                true -> 
-                    dispatch(CurSeq+1, RemainingWindow, ChannelID, Worker, Count+1);
-                false ->
-                    % Just return the existing sequence number. Nothing to do here.
-                    logger:debug("Dispatched ~p packets", [Count]),
-                    {CurSeq + 1, RemainingWindow}
-            end;
+            dispatch(NextSeq, RemainingWindow, ChannelID, Worker);
         _ ->
             % The first packet in the window is not the one we're looking for,
             % so just return.
-            logger:debug("Dispatched ~p packets", [Count]),
-            {CurSeq + 1, Window}
+            {NextSeq, Window}
     end.
+
+next(Seq) ->
+    % Must wrap at 16-bits.
+    (Seq + 1) rem ?MAX_SEQ_INDEX.
