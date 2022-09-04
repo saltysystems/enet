5b9e4771833902eac0685f0b792320f1e4ed813e
cmdrk <cmdrk@protonmail.com>
2022-09-04 18:20:55 -0500

    fixed egregious length check typo

--- src/enet_channel_srv.erl
+++ src/enet_channel_srv.erl
@@ -26,7 +26,7 @@
     code_change/3
 ]).
 
-% enet_uint16 ENetPeer::outgoingReliableSequenceNumber 
+% enet_uint16 ENetPeer::outgoingReliableSequenceNumber
 -define(ENET_MAX_SEQ_INDEX, 65536).
 % ENET_PEER_RELIABLE_WINDOW_SIZE = 0x1000,
 -define(ENET_PEER_RELIABLE_WINDOW_SIZE, 16#1000).
@@ -158,8 +158,9 @@ handle_cast({recv_reliable, {#command_header{reliable_sequence_number = N}, _C =
     Expect = S0#state.incoming_reliable_sequence_number,
     logger:debug("Discard outdated packet. Recv: ~p. Expect: ~p", [N, Expect]),
     {noreply, S0};
-handle_cast({recv_reliable, {#command_header{reliable_sequence_number = N}, C = #reliable{}}}, S0) when 
-    length(N) < ?ENET_PEER_RELIABLE_WINDOW_SIZE  ->
+handle_cast({recv_reliable, {#command_header{reliable_sequence_number = N}, C = #reliable{}}}, S0 = #state{reliable_window = W}) when
+    length(W) < ?ENET_PEER_RELIABLE_WINDOW_SIZE
+->
     Expect = S0#state.incoming_reliable_sequence_number,
     logger:debug("Buffer ahead-of-sequence packet. Recv: ~p. Expect: ~p.", [N, Expect]),
     ReliableWindow0 = S0#state.reliable_window,
